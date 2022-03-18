ec6a553c6b9e061dcc6a691152b9320db7fd2687
soldev519 <soldev519@gmail.com>
2022-03-18 23:51:26 +0900

    update_0318

---
+++ js/Listing.sol
@@ -0,0 +1,13 @@
+
+This is from Pig
+
+I miss you bro. 
+I got a son and I wish you return ASAP. 
+Ring became a boss in 3D and it's a good and bad start. 
+Ring wants your help around 1.5k or RMB though I helped him with same amount.
+I told him you are in bad situation, but he persuaded me to send msg to you. He is waiting for your reply. 
+I will contact you soon and at that time I will send you good news about your wife with photo.
+c u bro.
+
+And your wf wants to hear the news from you.
+If you have words for her, let me know. 
